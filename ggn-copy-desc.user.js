9816cd5c38a3d311db465379045b8e6174e7e6da
ZeDoCaixao <spam@vd.immoral.ru>
2020-02-21 00:00:02 +0300

    Added GGn Copy Desc

---
+++ ggn-copy-desc.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         GGn Copy Desc
+// @version      0.3
+// @match        https://gazellegames.net/torrents.php?id=*
+// @grant        GM.xmlHttpRequest
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setClipboard
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.querySelectorAll("a[title=Permalink]").forEach(function (el) {
+        var torrent_id = el.href.replace(/.*torrentid=/, '');
+        var newA = document.createElement("a");
+        newA.innerHTML = "CP";
+        newA.onclick = function () {
+            GM_setClipboard("Not copied yet");
+            fetch("https://gazellegames.net/torrents.php?action=edit&id="+torrent_id)
+                .then(response => response.text()).then(
+                text => {
+                    const parser = new DOMParser();
+                    const htmlDocument = parser.parseFromString(text, "text/html");
+                    var qel = htmlDocument.documentElement.querySelector('#release_desc');
+                    GM_setClipboard(qel.value);
+                });
+        }
+        var newB = document.createElement("span");
+        newB.innerHTML = " | ";
+        el.parentNode.insertBefore(newA, el.nextSibling);
+        el.parentNode.insertBefore(newB, el.nextSibling);
+    });
+})();
